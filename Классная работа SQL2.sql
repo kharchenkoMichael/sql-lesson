2b901060bbb2d557d6672ceb838d3a8e3c82eb6b
ZAMLER <112641549+ZAMLER@users.noreply.github.com>
2022-11-04 18:35:19 +0200

    Классная работа SQL2

---
+++ Классная работа SQL2.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE Games;
+USE Games;
+CREATE TABLE Users(
+Id INT PRIMARY KEY IDENTITY(1,1),
+Name NVARCHAR(MAX),
+Age INT,
+Phone NVARCHAR(MAX)
+);
+CREATE TABLE Games(
+Id INT PRIMARY KEY IDENTITY(1,1),
+Name NVARCHAR(MAX),
+Price MONEY,
+Genre NVARCHAR(MAX)
+);
+CREATE TABLE Orders(
+Id INT PRIMARY KEY IDENTITY(1,1),
+UsersId INT,
+GamesId INT,
+);
+
+INSERT INTO Users
+(Name,Age,Phone)
+values
+('Misha', 24,'0980023344'),
+('Jenya',27,'0980023111'),
+('Katya',23,'0939871234'),
+('Egor',18,'0982223455'),
+('Sasha',22,'0937771122')
+
+
+
+INSERT INTO Games
+(Name,Price,Genre)
+values
+('Cossaks',50,'Strategy'),
+('Call of Duty',150,'Action'),
+('World of Warcraft',200,'MMO'),
+('Dota2',300,'Strategy'),
+('Counter-Strike',500,'Action')
+
+INSERT INTO Orders
+(UsersId,GamesId)
+values
+(1,2),
+(2,5),
+(3,4),
+(4,3),
+(2,2)
+
+Select * From Users
+Select * From Games
+Select * From Orders
